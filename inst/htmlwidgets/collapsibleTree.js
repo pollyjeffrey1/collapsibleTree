227de35fccc719442fd9ec9d48a270432ae15cf2
Polly-Anne Jeffrey <122632327+pollyjeffrey1@users.noreply.github.com>
2023-08-22 14:28:59 +0100

    Update collapsibleTree.js

--- inst/htmlwidgets/collapsibleTree.js
+++ inst/htmlwidgets/collapsibleTree.js
@@ -11,7 +11,7 @@ HTMLWidgets.widget({
     options = {},
     treemap;
 
-    let myScale = d3.scaleSqrt().rangeRound([4,48]);
+    var myScale = d3.scaleSqrt().rangeRound([4,48]);
 
     // Optionally enable zooming, and limit to 1/5x or 5x of the original viewport
     var zoom = d3.zoom()
