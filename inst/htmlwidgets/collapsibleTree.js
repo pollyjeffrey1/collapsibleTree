319720182f8bd15d232838b754730c0d3c2cd9b1
Polly-Anne Jeffrey <122632327+pollyjeffrey1@users.noreply.github.com>
2023-08-22 10:56:55 +0100

    Update collapsibleTree.js

--- inst/htmlwidgets/collapsibleTree.js
+++ inst/htmlwidgets/collapsibleTree.js
@@ -95,8 +95,7 @@ HTMLWidgets.widget({
       .attr('x', 0)
       .attr('dy', '.35em')
       .attr('text-anchor', 'middle')
-      /*.style('font-size', options.fontSize + 'px')*/
-      .style('font-size', function(d) { return d3.scale.sqrt(d.data.WeightOfNode) + "px"; })
+      .style('font-size', options.fontSize + 'px')
       .text(function(d) { return d.data.WeightOfNode; });
 
       // UPDATE
